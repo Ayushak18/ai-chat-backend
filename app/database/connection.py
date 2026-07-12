e47b2e24618f179f748b53b84b5e37d4f971e042
Ayush Kamboj <aayushkamboj7@gmail.com>
2026-07-13 01:09:25 +0530

    Add:DB config and models added

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///chat.db"
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+from app.database.base import Base
+from app.database.connection import engine
+
 from app.api.auth import router as auth_router
 
+# Import models so they register with Base
+from app.database import models
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_router)
