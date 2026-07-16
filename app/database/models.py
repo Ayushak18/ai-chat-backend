9884f121389a1b7a5ba783776721135aa2f706e2
Ayush Kamboj <aayushkamboj7@gmail.com>
2026-07-16 22:44:54 +0530

    Add:Address added in the User Model

--- app/database/models.py
+++ app/database/models.py
@@ -11,3 +11,4 @@ class User(Base):
     email: Mapped[str] = mapped_column()
     password: Mapped[str] = mapped_column()
     phone: Mapped[str] = mapped_column(nullable=True)
+    address: Mapped[str] = mapped_column(nullable=True)
