6fe711c2162db93d1c66f1fadf19e57e19e4187a
Ayush Kamboj <aayushkamboj7@gmail.com>
2026-07-22 23:10:32 +0530

    Add:Auth added for route protection

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
-from app.dependencies.auth import get_auth_service
+from app.database.models import User
+from app.dependencies.auth import get_auth_service, get_current_user
 from app.repositories.user_repository import UserRepository
 from app.schemas.auth import LoginRequest, RegisterRequest
 from app.services.auth_service import AuthService
@@ -19,3 +20,15 @@ def register(
 @router.post("/login")
 def login(request: LoginRequest, auth_service: AuthService = Depends(get_auth_service)):
     return auth_service.login(request)
+
+
+@router.get("/me")
+def get_profile(current_user: User = Depends(get_current_user)):
+    return {
+        "message": " Welcome to the protected route!",
+        "user": {
+            "id": current_user.id,
+            "email": current_user.email,
+            "name": current_user.name,
+        },
+    }
