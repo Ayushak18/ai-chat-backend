414cbe8c40d06a6c388329d09c724eb11817ab27
Ayush Kamboj <aayushkamboj7@gmail.com>
2026-07-26 14:21:56 +0530

    Add:Create Message in repo

---
+++ app/repositories/messsage_repository.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from app.enum.message_role import MessageRole
+from app.database.models import Message
+
+
+class MessageRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_message(
+        self, content: str, conversation_id: int, role: MessageRole
+    ) -> Message:
+        new_message = Message(
+            content=content, conversation_id=conversation_id, role=role
+        )
+        self.db.add(new_message)
+        self.db.commit()
+        self.db.refresh(new_message)
+        return new_message
