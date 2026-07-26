756be554df0138ff112a1f29b3d894edf4377095
Ayush Kamboj <aayushkamboj7@gmail.com>
2026-07-26 15:30:42 +0530

    Add:Message service for apis

---
+++ app/services/message_service.py
@@ -0,0 +1,20 @@
+from app.enum.message_role import MessageRole
+from app.repositories.messsage_repository import MessageRepository
+from app.database.models import Message
+
+
+class MessageService:
+    def __init__(self, message_repository: MessageRepository):
+        self.message_repository = message_repository
+
+    def create_message(
+        self, content: str, conversation_id: int, role: MessageRole
+    ) -> Message:
+        message = self.message_repository.create_message(content, conversation_id, role)
+        return message
+
+    def get_messages_by_conversation_id(self, conversation_id: int) -> list[Message]:
+        messages = self.message_repository.get_messages_by_conversation_id(
+            conversation_id
+        )
+        return messages
